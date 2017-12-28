c63fe80c39af8969c0a082e87f16d47a2bf17ac0
gabrielfcdossantos <gabrielfcdossantos@gmail.com>
2017-12-28 15:25:32 -0200

    Primeiro HTML adicionado no site.

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+from django.http import HttpResponse
+# Create your views here.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html')
